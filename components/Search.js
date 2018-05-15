b83b515ada7fd36afaf95e6364a63310d78fff48
bradserlis <bradserlis@yahoo.com>
2018-05-15 09:05:56 -0700

    styling

--- components/Search.js
+++ components/Search.js
@@ -79,7 +79,7 @@ updateCommentsList = (newComment, key) => {
           <DeckSwiper
             dataSource={this.state.posts}
             renderItem={item =>
-              <Card style={{ elevation: 5, height:470, backgroundColor:'rgba(150,150,150,.8)'}}>
+              <Card style={{ elevation: 5, marginTop:10, height:470, backgroundColor:'rgba(150,150,150,.8)'}}>
                 <CardItem>
                   <Left>
                     <Body>
